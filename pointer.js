721f747fa50596b27f92f6c5ca4352340749a558
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-09-05 23:24:16 -0700

    background circle square animations

--- pointer.js
+++ pointer.js
@@ -4,7 +4,6 @@ addEventListener("mousemove", (event) => {
     const di = document.querySelectorAll(".pointer");
     if (di === null) return;
     di.forEach((pointer) => {
-        console.log(pointer.classList);
         let o = 34;
         if (pointer.classList.contains("inner")) {
             o = 10;
@@ -13,4 +12,22 @@ addEventListener("mousemove", (event) => {
         pointer.style.left = `${event.x - o}px`;
         pointer.style.top = `${event.y - o}px`;
     });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    let circles = document.querySelector(".circles");
+
+    for (let i = 0; i < 12; ++i) {
+        let circle = document.createElement("div");
+        circle.style.left = `${Math.floor(Math.random() * 100)}vw`;
+        let size = Math.floor(Math.random() * 80 + 20);
+        circle.style.width = `${size}px`;
+        circle.style.height = `${size}px`;
+        circle.style.animationDelay = `${Math.random() * 20}s`;
+        circle.style.animationDuration = `${Math.random() * 25 + 10}s`;
+        circle.style.backgroundColor = `color-mix(in srgb, #fd6d0e ${Math.floor(
+            Math.random() * 50 + 50
+        )}%, black)`;
+        circles?.appendChild(circle);
+    }
 });
